2cb9acf0f4cd116a77d6d42924ffa3ad992f02ef
tejasdesai <tejasdesai0531@gmail.com>
2021-09-24 14:00:13 +0000

    Adds tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -14,7 +14,7 @@ const start = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
     console.log('Connected to MongoDb');
   } catch (err) {
